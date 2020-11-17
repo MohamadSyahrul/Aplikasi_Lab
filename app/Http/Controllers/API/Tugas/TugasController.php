3742acfcd0ac9f193825a7937a62f1f71ea3c71a
MohamadSyahrul <mochamadsahrul28@gmail.com>
2020-11-18 00:35:28 +0700

    membuat controller tugas

--- app/Http/Controllers/API/Tugas/TugasController.php
+++ app/Http/Controllers/API/Tugas/TugasController.php
@@ -4,8 +4,26 @@ namespace App\Http\Controllers\API\Tugas;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Task;
 
 class TugasController extends Controller
 {
-    //
+    public function store(Request $request){
+        $validateData = $request->validate([
+            'id' => 'required',
+            'Nama_tugas' => 'required',
+            'id_kategori' => 'required',
+            'Ket_tugas' => 'required',
+            'Status_tugas' => 'required',
+        ]);
+        $data = new Task;
+        $data->id = $request->id;
+        $data->Nama_tugas = $request->Nama_tugas;
+        $data->id_kategori = $request->id_kategori;
+        $data->Ket_tugas = $request->Ket_tugas;
+        $data->Status_tugas = $request->Status_tugas;
+        
+        $data->save();
+        return response()->json($data, 201);
+    }
 }
